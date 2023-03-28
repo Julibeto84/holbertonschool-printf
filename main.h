7d50632b61f6e3a96f4db079aff8ded5044e70a2
Gaburon <japo000@hotmail.com>
2023-03-28 09:57:36 -0700

    prueba

---
+++ main.h
@@ -0,0 +1,30 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stddef.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * struct cases - Struct for print cases
+ * @pfcase: character for case
+ * @convfun: Conversion function
+ */
+
+typedef struct cases
+{
+	char *pfcase;
+	int (*convfun)(va_list n, char *buff, int print_len);
+} print_cases;
+
+char *_itoa(int num, char *s, unsigned int base);
+char *_utoa(unsigned int num, char *s, unsigned int base);
+char *_utoah(unsigned int num, char *s, unsigned int base);
+int _printf(char *format, ...);
+int replace(char *buffer, char *s, int print_len);
+int (*get_print_cases(char *arg))(va_list, char *, int);
+int c_case(va_list, char *, int);
+int s_case(va_list, char *, int);
+int perc_case(va_list, char *, int);
+
+#endif
