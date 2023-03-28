097e209b5a5e904800ec19e294b3996d572c8c4e
Gaburon <japo000@hotmail.com>
2023-03-28 11:11:51 -0700

    s

--- cases1.c
+++ cases1.c
@@ -80,3 +80,54 @@ int dec_case(va_list ap, char *buff, int print_len)
 	free(aux);
 	return (print_len);
 }
+/**
+ * perc_case - function for % case
+ * @ap: Unused
+ * @buff: buffer
+ * @print_len: Actual position of buff
+ * Return: Last position on buff
+ */
+
+int perc_case(va_list __attribute__((unused)) ap, char *buff, int print_len)
+{
+	char *c;
+
+	c = malloc((sizeof(char) + 1));
+	if (c == NULL)
+		return (1);
+	c[0] = '%';
+	c[1] = '\0';
+	print_len = replace(buff, c, print_len);
+	free(c);
+	return (print_len);
+}
+
+/**
+ * bin_case - function for binary case
+ * @ap: intiger to convert into binary
+ * @buff: buffer
+ * @print_len : actual position of buff
+ * Return: Last position on buff
+ */
+
+int bin_case(va_list ap, char *buff, int print_len)
+{
+	char *s;
+	char *aux;
+	int s_len, num;
+
+	num = va_arg(ap, int);
+	aux = malloc(sizeof(char) * 35);
+	if (aux == NULL)
+		return (1);
+	aux = _itoa(num, aux, 2);
+	s_len = _strlen(aux);
+	s = malloc((sizeof(char) * s_len) + 1);
+	if (s == NULL)
+		return (1);
+	_strcpy(s, aux);
+	print_len = replace(buff, s, print_len);
+	free(s);
+	free(aux);
+	return (print_len);
+}
--- string_functions.c
+++ string_functions.c
@@ -41,25 +41,6 @@ char *_strcpy(char *dest, char *src)
 }
 
 /**
- * _strcpy - copies the string pointed to by src, including the terminating
- * null byte (\0), to the buffer pointed to by dest.
- * @dest: Pointer to destiny
- * @src: Pointer to source
- * Return: pointer to dest
- */
-
-char *_strcpy(char *dest, char *src)
-{
-	int len;
-	int i;
-
-	len = _strlen(src);
-	for (i = 0; i <= len; i++)
-		*(dest + i) = *(src + i);
-	return (dest);
-}
-
-/**
  * rev_string - reverses a string.
  * @s: Pointer to string to reverse
  * Return: Always 0
